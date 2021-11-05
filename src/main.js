4ac76af9dc0ac20c3f1ed4091375c8a7ea46ead0
SunOn9 <lehophattai@gmail.com>
2021-11-05 18:02:10 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import router from "./router";
 
-createApp(App).mount('#app')
+import { handle } from "./common/promise";
+createApp(App)
+    .mixin({
+        methods: {
+            handle,
+    }
+})
+.use(router).mount('#app');
+
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from "vue-router";
+const routes = [
+    {
+    path: "/",
+    alias: "/contactbook",
+    name: "ContactBook",
+    component: () => import("../views/ContactBook"),
+    },
+    {
+    path: "/contacts/:id",
+    name: "EditContact",
+    component: () => import("../views/ContactEdit"),
+    },        
+];    
+const router = createRouter({
+history: createWebHistory(),
+routes,
+});
+export default router;
